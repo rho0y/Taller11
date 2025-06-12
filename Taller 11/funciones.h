de4e56b7c5e28e0b71bef35fb842d0c3aad8a980
rho0y <tomas.gonzalez@udla.edu.ec>
2025-06-11 23:27:12 -0500

    Update funciones.h

--- Taller 11/funciones.h
+++ Taller 11/funciones.h
@@ -1,57 +1,36 @@
-#define MAX_LIBROS 100
+#define MAX_LIBROS 10
 #define MAX_ID 20
 #define MAX_TITULOS 100
 
-typedef struct
+struct InfoAutor
 {
-    char nombre[50];
-    char apellido[50];
-    char nacionalidad[30];
-    int edad;
-} InfoAutor;
+    char nombreCompleto[100];
+};
 
-typedef struct
-{
-    int año;
-    char editorial[50];
-    char ciudad[30];
-    char pais[30];
-    int numeroEdicion;
-} InfoPublicacion;
-
-typedef struct
+struct EstadoLibro
 {
     int disponible;
     char fechaPrestamo[20];
     char fechaDevolucion[20];
     char nombrePrestatario[50];
     char contactoPrestatario[50];
-} EstadoLibro;
+};
 
-typedef struct
-{
-    char genero[30];
-    char categoria[30];
-    char subcategoria[30];
-    int clasificacionEdad;
-} CategorizacionLibro;
-
-typedef struct
+struct DatosLibroAnidado
 {
     char ID[MAX_ID];
     char titulo[MAX_TITULOS];
-    InfoAutor autor;
-    InfoPublicacion publicacion;
-    EstadoLibro estado;
-    CategorizacionLibro categoria;
-    int numeroPaginas;
     char idioma[20];
-} DatosLibroAnidado;
+    char genero[30];
+    struct InfoAutor autor;
+    struct EstadoLibro estado;
+};
 
 void menu();
-int idExiste(DatosLibroAnidado coleccion[], int totalLibros, const char *id);
-void registrarLibros(DatosLibroAnidado coleccion[], int *totalLibros);
-void listaLibros(DatosLibroAnidado coleccion[], int totalLibros);
-void buscarLibro(DatosLibroAnidado coleccion[], int totalLibros);
-void actualizarEstadoLibro(DatosLibroAnidado coleccion[], int totalLibros);
-void eliminarLibro(DatosLibroAnidado coleccion[], int *totalLibros, int indice);
+int idExiste(struct DatosLibroAnidado coleccion[], int totalLibros, const char *id);
+void registrarLibros(struct DatosLibroAnidado coleccion[], int *totalLibros);
+void listaLibros(struct DatosLibroAnidado coleccion[], int totalLibros);
+void buscarLibro(struct DatosLibroAnidado coleccion[], int totalLibros);
+void actualizarEstadoLibro(struct DatosLibroAnidado coleccion[], int totalLibros);
+void eliminarLibro(struct DatosLibroAnidado coleccion[], int *totalLibros, int indice);
+int validarCadenaVacia(const char *cadena);
